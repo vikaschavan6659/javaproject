35537cf49d263279284db7baaeb01da1ca76b714
vikaschavan6659 <105354011+vikaschavan6659@users.noreply.github.com>
2022-05-11 16:34:15 +0530

    error is cleared
    bug cleared

--- firstpro.java
+++ firstpro.java
@@ -3,6 +3,6 @@ public class firstpro {
 	public static void main(String args[])
 	{	  
 		  System.out.println("Hello Developers");  
-		 System.out.println("Hello my dera frinds);
+		 System.out.println("Hello my dera frinds");
 	}
 }
