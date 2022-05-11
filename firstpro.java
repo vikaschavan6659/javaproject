79307128cc856427c5e8292a1cb4d7facf8791ed
VARADHARAJAN4 <85033135+VARADHARAJAN4@users.noreply.github.com>
2022-05-11 16:36:32 +0530

    Update firstpro.java

--- firstpro.java
+++ firstpro.java
@@ -2,7 +2,9 @@ import java.io.*;
 public class firstpro {
 	public static void main(String args[])
 	{	  
-		  System.out.println("Hello Developers");  
-		 System.out.println("Hello my dera frinds");
+		//  System.out.println("Hello Developers");  
+		 //System.out.println("Hello my dera frinds");
+		your project good
+		
 	}
 }
