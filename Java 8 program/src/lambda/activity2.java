0c760c76db9d73adb91deb9fba4a139dc3145442
vikas <vikas@LAPTOP-MLARCVM1>
2022-05-13 16:54:28 +0530

    im gating that one

---
+++ Java 8 program/src/lambda/activity2.java
@@ -0,0 +1,18 @@
+package lambda;
+
+public class activity2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				int a=15,b=20,sum=0;
+				System.out.println("num1="+a);
+				System.out.println("num2="+b);
+				sum=a+b;
+				System.out.println("the sum="+sum);
+				sub su1=(c,d)->(c*d);
+				System.out.println(su1.su(20,10));
+				
+	}
+
+}
